f205a0f6a7d399df21cc7aceac99e4c6f6f3a07b
SHUBHAM-NAGDIYA <143119328+SHUBHAM-NAGDIYA@users.noreply.github.com>
2026-02-24 14:19:25 +0530

    Uncomment SECRET_KEY assignment in settings.py

--- main/main/settings.py
+++ main/main/settings.py
@@ -1,6 +1,5 @@
 import os
 import dj_database_url
-
 #SECRET_KEY = os.environ.get('J-2F5WVva_U4Hp0Geki_TE2SM0d04gurUlLe62NclCF1ixUcZmN5tIGmmOMnxg1Bafg')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
