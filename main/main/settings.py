8ace83d6aa066458d044e1f7d1551e0f76429196
SHUBHAM-NAGDIYA <143119328+SHUBHAM-NAGDIYA@users.noreply.github.com>
2026-02-24 14:39:37 +0530

    Add ROOT_URLCONF setting to settings.py

--- main/main/settings.py
+++ main/main/settings.py
@@ -3,7 +3,7 @@ import dj_database_url
 #SECRET_KEY = os.environ.get('J-2F5WVva_U4Hp0Geki_TE2SM0d04gurUlLe62NclCF1ixUcZmN5tIGmmOMnxg1Bafg')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-
+ROOT_URLCONF = 'main.urls'
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 DATABASES = {
