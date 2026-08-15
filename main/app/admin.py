6ab51dac02bf532e3fd5e50cc395451c3f51b87e
Shubham Nagdiya <143119328+SHUBHAM-NAGDIYA@users.noreply.github.com>
2026-08-15 17:42:08 +0530

    Delete main directory

--- main/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import StoreOwneres,Sales,Product
-
-# Register your models here.
-admin.site.register(StoreOwneres)
-admin.site.register(Product)
-admin.site.register(Sales)
